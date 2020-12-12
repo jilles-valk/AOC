9215a986e407005ed97f6db23b31bedf615a19e3
Jilles Valk <jilwa@outlook.com>
2020-12-12 16:24:56 +0100

    Part two of day 11 done

--- AOC11/AOC11.cpp
+++ AOC11/AOC11.cpp
@@ -35,7 +35,38 @@ std::vector<std::vector<Position>> GetInput(std::string inputFileName)
     return waitingArea;
 }
 
-bool CheckHasTargetNum(std::vector<std::vector<Position>>& waitingArea, int iStart, int jStart, Position checkPos, int targetCheckPos, bool targetIsMax)
+bool CheckDirection(std::vector<std::vector<Position>>& waitingArea, Position checkPos, Position notPos, int iStart, int jStart, int deltaI, int deltaJ)
+{
+    int i = 0, j = 0, maxI = 0, maxJ = 0, max = 0;
+
+    if (deltaI < 0)
+        maxI = iStart + 1;
+    else if (deltaI > 0)
+        maxI = waitingArea.size() - iStart;
+
+    if (deltaJ < 0)
+        maxJ = jStart + 1;
+    else if (deltaJ > 0)
+        maxJ = waitingArea[0].size() - jStart;
+
+    max = deltaI == 0 ? maxJ : deltaJ == 0 ? maxI : std::min(maxI, maxJ);
+
+    for (int t = 1; t < max; t++)
+    {
+        i = deltaI * t;
+        j = deltaJ * t;
+
+        if (waitingArea[iStart + i][jStart + j] == notPos)
+            return false;
+
+        if (waitingArea[iStart + i][jStart + j] == checkPos)
+            return true;
+    }
+
+    return false;
+}
+
+bool CheckHasTargetNum(std::vector<std::vector<Position>>& waitingArea, int iStart, int jStart, Position checkPos, int targetCheckPos, bool targetIsMax, bool checkLine = false)
 {
     int waISize = waitingArea.size();
     int waJSize = waitingArea.back().size();
@@ -47,7 +78,8 @@ bool CheckHasTargetNum(std::vector<std::vector<Position>>& waitingArea, int iSta
         {
             if (i == 0 && j == 0)
                 continue;
-            if (waitingArea[iStart + i][jStart + j] == checkPos)
+
+            if (checkLine ? CheckDirection(waitingArea, Position::OccupiedSeat, Position::EmptySeat, iStart, jStart, i , j ) : waitingArea[iStart + i][jStart + j] == checkPos)
             {
                 numCheckPos++;
                 if (numCheckPos > targetCheckPos && targetIsMax)
@@ -60,10 +92,10 @@ bool CheckHasTargetNum(std::vector<std::vector<Position>>& waitingArea, int iSta
         }
     }
 
-    return targetIsMax ? true : false;
+    return targetIsMax;
 }
 
-int SeatsAfterStabilisedChaos(std::vector<std::vector<Position>>& waitingArea)
+int SeatsAfterStabilisedChaos(std::vector<std::vector<Position>> waitingArea, int maxNumOccupied, bool checkLine = false)
 {
     bool changed = false;
     Position curPos = Position::Nothing;
@@ -78,20 +110,32 @@ int SeatsAfterStabilisedChaos(std::vector<std::vector<Position>>& waitingArea)
             {
                 curPos = waitingArea[i][j];
 
-                if (curPos == Position::EmptySeat && CheckHasTargetNum(waitingArea, i, j, Position::OccupiedSeat, 0, true))
+                if (curPos == Position::EmptySeat && CheckHasTargetNum(waitingArea, i, j, Position::OccupiedSeat, 0, true, checkLine))
                 {
                     changed = true;
                     tempWaitingArea[i][j] = Position::OccupiedSeat;
                 }
-                else if (curPos == Position::OccupiedSeat && CheckHasTargetNum(waitingArea, i, j, Position::OccupiedSeat, 4, false))
+                else if (curPos == Position::OccupiedSeat && CheckHasTargetNum(waitingArea, i, j, Position::OccupiedSeat, maxNumOccupied, false, checkLine))
                 {
                     changed = true;
                     tempWaitingArea[i][j] = Position::EmptySeat;
                 }
+
+                //if (curPos == Position::OccupiedSeat)
+                //    std::cout << 'X';
+                //if (curPos == Position::EmptySeat)
+                //    std::cout << 'L';
+                //if (curPos == Position::Nothing)
+                //    std::cout << '.';
+                
+                
             }
+            //std::cout << std::endl;
         }
 
         waitingArea = tempWaitingArea;
+
+        //std::cout << std::endl;
 
     } while (changed);
 
@@ -103,7 +147,11 @@ int main()
 {
     auto waitingArea = GetInput("input.txt");
 
-    int numSeatsAfterStabilesedChaos = SeatsAfterStabilisedChaos(waitingArea);
+    int numSeatsAfterStabilesedChaos = SeatsAfterStabilisedChaos(waitingArea, 4);
 
     std::cout << "Number of seats occupied after chaos has stabilised: " << numSeatsAfterStabilesedChaos << std::endl;
+
+    numSeatsAfterStabilesedChaos = SeatsAfterStabilisedChaos(waitingArea, 5, true);
+
+    std::cout << "Number of seats occupied after chaos has stabilised with new rules: " << numSeatsAfterStabilesedChaos << std::endl;
 }
